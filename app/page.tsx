6faf60a088423d0ccc5c4e68b61a41051d88a486
David Umoh <daveumoh@amazon.com>
2024-05-15 11:06:01 +0300

    changes to pass role and level dynamically to the llm

--- app/page.tsx
+++ app/page.tsx
@@ -32,15 +32,22 @@ export default function App() {
   const [answers, setAnswers] = useState<Answers>({});
   const [generatedPlan, setGeneratedPlan] = useState<string>("")
   const [loading, setLoading] = useState<boolean>(false)
+  const [role, setRole] = useState<string>("")
+  const [level, setLevel] = useState<string>("")
+  const [plan, setPlan] = useState<string>("")
 
   const handleInputChange = (questionId: number, value: string) => {
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
       [questionId]: value,
     }));
-
   };
-  function listLearningPlans() {
+ 
+  const handleRoleChange  = (role: string) => {
+    setRole(role)
+  }
+
+  const listLearningPlans = () => {
     client.models.LearningPlan.observeQuery().subscribe({
       next: (data) => setLearningPlans([...data.items]),
     });
@@ -50,29 +57,32 @@ export default function App() {
     listLearningPlans();
   }, []);
 
-  function createLearningPlan(event:any) {
+  const createLearningPlan = async () => {
     //let plan = event.target.value
    setLoading(true) 
-    client.models.LearningPlan.create({
-      role: 'software',
-      level: 'junior',
-      plan: generatedPlan,
+     const createdPlan =  await client.models.LearningPlan.create({
+      role: role,
+      level: level,
+      plan: plan,
       status: 'initial',
     });
 
     setLoading(false) 
+    return createdPlan
   }
 
-  async function fetchQuestions(e: any) {
+  const fetchQuestions = async (level: string) => {
     setLoading(true)
+    setLevel(level)
+  
 
     //connect with llm and generate questions for role and level
     const response = await client.queries.askBedrock(
-      { prompt: `To enable us generate a more relevant learning plan for a senior level software engineer, give me questions we can ask the user. Answers to this question will be fed back to an llm to generate a learning plan. Return 10 questions in the following format:
+      { prompt: `To enable us generate a more relevant learning plan for a ${level} ${role} give me questions we can ask the user. Answers to this question will be fed back to an llm to generate a learning plan. Return 10 questions in the following format:
       {"questions": [
         {
           "id": 1,
-          "text": "What is your current level of expertise in software engineering?"
+          "text": "What is your current level of expertise?"
         },
         {
           "id": 2,
@@ -87,9 +97,9 @@ export default function App() {
       setLoading(false)
   }
 
-  async function generatePlan(event: any){
+  const generatePlan = async (event: any) => {
    setLoading(true);
-    let answersString = "Given the following questions and answers, generate a learning plan for a senior software engineer:";
+    let answersString = `Given the following questions and answers, generate a learning plan for a ${level} ${role}:`;
     for (const [questionId, answer] of Object.entries(answers)) {
       let question = questions.find((q) => q.id === parseInt(questionId));
       if (!question) continue;
@@ -101,7 +111,12 @@ export default function App() {
       const res = JSON.parse(response.data?.body!);
       const content = res.content[0].text; 
       setGeneratedPlan(content)
+      setPlan(content)
       setLoading(false);
+  }
+
+  const handleModifiedPlan = (event: any) => {
+      setPlan(event.target.value)
   }
 
   return (
@@ -116,13 +131,14 @@ export default function App() {
         label="Role"
         options={['','Cloud Architect','Software Engineer', 'Product Designer']}
         placeholder="Select a role"
+        onChange={(event)=>handleRoleChange(event.target.value)}
         ></SelectField>
         
         <SelectField
         label="Level"
         options={['', 'Junior','Mid', 'Senior']}
         placeholder="Select a level"
-        onChange={fetchQuestions}
+        onChange={(event) => fetchQuestions(event.target.value)}
         ></SelectField>
        
       <Loader display={loading?"block":"none"} variation="linear"  />
@@ -141,19 +157,16 @@ export default function App() {
 <div hidden={!generatedPlan}>
   <Heading level={5}>Generated Plan</Heading>
   <Flex direction="column"  gap="medium">
-    <TextAreaField label="" defaultValue={generatedPlan} row="10"></TextAreaField>
+    <TextAreaField label="" defaultValue={generatedPlan} row="10" onChange={(event)=> handleModifiedPlan(event.target.value)}></TextAreaField>
   <button onClick={createLearningPlan}>Save Plan</button>
-
 </Flex>  
 </div>   
      </Card>
-
      
       <h1>My LearningPlans</h1>
-
       <ul>
         {learningPlans.map((learningPlan) => (
-          <li key={learningPlan.id}>{learningPlan.role}</li>
+          <li key={learningPlan.id}>{learningPlan.role} - {learningPlan.level}</li>
         ))}
       </ul>
       </div>
